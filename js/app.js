523c3038ef3e2de69dcb6d73c82ba8e9637ae05d
FeedTheMind <FeedTheMind@users.noreply.github.com>
2017-02-07 19:25:27 -0500

    Use JS and jQuery to create and to add HTML

--- js/app.js
+++ js/app.js
@@ -7,5 +7,14 @@
   // Technically, not needed, as script is before closing </body>
     // But good practice
 $(document).ready(function () {
+  // Use JS to create HTML element
+  var searchBox = "<h2>Employees</h2> <div class='employeeSearch'> <input class='searchInput' placeholder='Search for employees.'></div>";
+  // 10 is placeholder, for now
+  var employeesPerPage = 10;
+  // Use clone to make a deep copy of employeeList for later use
+  var listClone =  $('.employeeList > li').clone();
+  // Use jQuery to add said HTML
+  $('.pageHead').html(searchBox);
 
+  $('.pageContainer').append("<div class='pagination'></div>");
 });
