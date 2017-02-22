d751cced74288aeb817dee08e815fdf6f894fd2d
FeedTheMind <FeedTheMind@users.noreply.github.com>
2017-02-21 20:01:09 -0500

    Use JS and jQuery to search for a name

--- js/app.js
+++ js/app.js
@@ -6,15 +6,39 @@
 // Prevent code from executing before it's ready
   // Technically, not needed, as script is before closing </body>
     // But good practice
-$(document).ready(function () {
+$(document).ready( () => {
+  // Store array of employee names with selector
+  var employeeNames = $('.employeeDetails > h3');
+
+  // Create array of text data from HTML elements
+  const lowerCaseNames = [];
+
+  for (var i = 0; i < employeeNames.length; i++) {
+    lowerCaseNames.push(employeeNames[i].textContent.toLowerCase());
+  }
+
   // Use JS to create HTML element
   var searchBox = "<h2>Employees</h2> <div class='employeeSearch'> <input class='searchInput' placeholder='Search for employees.'></div>";
-  // 10 is placeholder, for now
-  var employeesPerPage = 10;
-  // Use clone to make a deep copy of employeeList for later use
-  var listClone =  $('.employeeList > li').clone();
+
   // Use jQuery to add said HTML
   $('.pageHead').html(searchBox);
+  var $search = $('.searchInput');
 
-  $('.pageContainer').append("<div class='pagination'></div>");
-});
+  $search.keyup(() => {
+    var $searchVal = $search.val().trim();
+
+    if ($searchVal === '') {
+      $search.removeClass('changeInput');
+    } else {
+      $search.addClass('changeInput');
+
+      for (var i = 0; i < lowerCaseNames.length; i++) {
+        if ($searchVal.toLowerCase() === lowerCaseNames[i]) {
+          $(employeeNames[i]).addClass('matchedName');
+        } else {
+          $(employeeNames[i]).removeClass('matchedName');
+        }
+      }
+    }
+  });
+}); // document function
